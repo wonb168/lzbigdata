fd89ed98dfc219726d44acdd20478fdf3da0f43d
wangchangzhen <wangchangzhen@linezonedata.com>
2019-08-12 19:50:40 +0800

    0.1

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup
+setup(
+    name = 'lzbigdata',      
+    version = '0.1',
+    py_modules = ['bigdata'],
+    author='wangcz',
+    author_email='wangchangzhen@linezonedata.com',
+    url = 'https://github.com/wonb168/lzbigdata.git',
+    description = '览众大数据组件包'   
+    )
+
+# setup(
+#     name='lzbigdata',
+#     version=0.1,
+#     description=(
+#         '览众大数据组件包'
+#     ),
+#     author='wangcz',
+#     author_email='wangchangzhen@linezonedata.com',
+#     maintainer='wangcz',
+#     maintainer_email='wangchangzhen@linezonedata.com',
+#     platforms=["all"],
+#     url='http://192.168.16.236:8999/wangchangzhen/lzbigdata.git',
+# )
+# from setuptools import setup, find_packages
+# setup(
+#     name = '下载的模块名',      
+#     version = '1.1.0',
+#     py_modules = ['你的轮子名'],
+#     author = '注册的账号',        
+#     author_email = '注册的游戏',
+#     url = '一个地址，比如github轮子下载地址啥的 ',
+#     description = '轮子的描述'   
+#     )
